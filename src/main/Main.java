a032a17b70db50fa8b28781df71dc7b0567aae9b
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-13 21:19:14 +0300

    collection Java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,30 +2,14 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        String[] employees = {
-                "Саша",
-                "Коля",
-                "Вася",
-                "Толя",
-                "Иван"
-        };
-        String[] second = new String[employees.length + 1 ];
-        for (int i = 0; i < employees.length;i++){
-            second[i] = employees[i];
+        MyArrayList employees = new MyArrayList();
+        employees.add("Alex");
+        employees.add("Serg");
+        employees.add("Ola");
+        employees.add("Khan");
+        employees.add("Zula");
+        for (int i = 0; i < employees.getSize();i++) {
+            System.out.println(employees.get(i));
         }
-        second[second.length -1] = "Зуля";
-        employees = second;
-        String[] newNames = new String[employees.length -1];
-        for (int i = 0,j = 0; i < newNames.length;i++){
-            if(employees[i] != null){
-                newNames[j] = employees[i];
-                j++;
-            }
-        }
-        employees = newNames;
-        for (String name: employees) {
-            System.out.println(name);
-        }
-
     }
 }
