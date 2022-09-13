fa4dd1bf953e34bf9ad7dee7923e26a1f7f2fbbf
ragioniere1980@gmail.com <ragioniere1980@gmail.com>
2022-09-13 22:20:46 +0300

    collection Java

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,22 +2,15 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        byte c;
-        Byte cl;
-        short s;
-        Short sl;
-        int a = 5;
-        Integer b = 5;
-        long l;
-        Long ll;
-        char ch;
-        Character chl;
-        float f;
-        Float fl;
-        double d;
-        Double dl;
-        boolean bool;
-        Boolean boll;
+        String s = "1000";
+        String test = "2000";
+        int a = Integer.parseInt(s);
+        int b = Integer.parseInt(test);
+        System.out.println(a + b);
+        String ss = "false";
+        boolean tt = Boolean.parseBoolean(ss);
+        System.out.println(tt);
+
         /*
         * С маленькой буквы примитив, с большой оболочка,ссылочный тип
         * */
